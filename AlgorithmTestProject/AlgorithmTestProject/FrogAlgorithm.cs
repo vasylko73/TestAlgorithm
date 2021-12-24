998fc2ca8b1ae66095eec554eaa3478656c5e7c8
vasyl <60632175+vasylko73@users.noreply.github.com>
2021-12-24 12:47:45 +0200

    Add Algorithm

---
+++ AlgorithmTestProject/AlgorithmTestProject/FrogAlgorithm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTestProject
+{
+    class FrogAlgorithm
+    {
+        public int findShortestWay((int, int) startPosition, (int, int) endPosition, int[,] arr)
+        {
+            int positionX = startPosition.Item1;
+            int positionY = startPosition.Item2;
+            int count = 0;
+
+            while (endPosition.Item2 - positionY != 0 || endPosition.Item1 - positionX != 0)
+            {
+                int lengthByX = endPosition.Item1 - positionX;
+                int lengthByY = Math.Abs(endPosition.Item2 - positionY);
+
+                if (lengthByX == 0)//умова коли ми знаходимось на одному кільці з кінцевою точкою
+                {
+                    int residual = lengthByY % 3;
+                    if (residual == 0 && !(arr[positionX, positionY] == 1))//умова коли нам треба стрибнути на 3 позиції вперед
+                    {
+                        positionY += 3;
+                    }
+                    else if (residual == 0 && arr[positionX, positionY + 3 > 16 ? positionY - 16 : positionY] == 1)//умова коли нам треба стрибнути і оминути перешкоду
+                    {
+
+                        if (positionX > 2 && positionX <= 16 && !(arr[positionX-2, positionY + 1 > 16 ? positionY - 16 : positionY] == 1))
+                        {
+                            positionX -= 2;
+                            positionY += 1;
+                        }
+                        else if (positionX >= 0 && positionX < 15 && !(arr[positionX + 2, positionY + 1 > 16 ? positionY - 16 : positionY] == 1))
+                        {
+                            positionX += 2;
+                            positionY += 1;
+                        }
+                    }
+                    else if (residual == 1)//умова коли треба стрибнути на 4 позиції вперед
+                    {
+                        if (positionX > 1 && positionX <= 16 && !(arr[positionX - 1, positionY + 2 > 16 ? positionY - 16 : positionY] == 1))
+                        {
+                            positionX -= 1;
+                            positionY += 2;
+                        }
+                        else if (positionX >= 0 && positionX < 16 && !(arr[positionX  +1 , positionY + 2 > 16 ? positionY - 16 : positionY] == 1))
+                        {
+                            positionX += 1;
+                            positionY += 2;
+                        }
+                    }
+                    else if (residual == 2)
+                    {
+                        if (positionX > 2 && positionX <= 16 && !(arr[positionX - 2, positionY + 1 > 16 ? positionY - 16 : positionY] == 1))
+                        {
+                            positionX -= 2;
+                            positionY += 1;
+                        }
+                        else if (positionX >= 0 && positionX < 15 && !(arr[positionX + 2, positionY + 1 > 16 ? positionY - 16 : positionY] == 1))
+                        {
+                            positionX += 2;
+                            positionY += 1;
+                        }
+                    }
+
+                }
+                else if (lengthByX < 0)//умова коли кінцева точка знаходиться нижче по Х ніж позиція жаби
+                {
+                    int residual = Math.Abs(lengthByX % 2);
+
+                    if (residual == 0 && !(arr[positionX - 2, positionY + 1 > 16 ? positionY - 16 : positionY] == 1))//умова коли нам треба стрибнути на 2 позиції вниз
+                    {
+                        positionX -= 2;
+                        positionY += 1;
+                    }
+                    else //умова коли нам треба стрибнути на 1 позиції вниз
+                    {
+                        positionX -= 1;
+                        positionY += 2;
+                    }
+                }
+                else if (lengthByX > 0)//умова коли кінцева точка знаходиться вище по Х ніж позиція жаби
+                {
+                    int residual = lengthByX % 2;
+                    if (residual == 0 && !(arr[positionX + 2, positionY + 1 > 16 ? positionY - 16 : positionY] == 1))//умова коли нам треба стрибнути на 2 позиції вгору
+                    {
+                        positionX += 2;
+                        positionY += 1;
+                    }
+                    else//умова коли нам треба стрибнути на 1 позиції вгору
+                    {
+                        positionX += 1;
+                        positionY += 2;
+                    }
+                }
+                if (positionY > 16)
+                {
+                    positionY -= 16;
+                }
+                count++;
+                if (count == 1000)
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ AlgorithmTestProject/AlgorithmTestProject/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10, 16];
+            arr[4, 6] = 1;
+            arr[5, 3] = 1;
+            arr[7, 6] = 1;
+            arr[9, 3] = 1;
+            arr[3, 9] = 1;
+            arr[3, 3] = 1;
+            var frogTask = new FrogAlgorithm();
+            var countOfClimb = frogTask.findShortestWay((1,9),(7,13),arr);
+            if(countOfClimb==1000)
+            {
+                Console.WriteLine("Cannot be found");
+            }
+            else
+            {
+                Console.WriteLine($"Count of climb: {countOfClimb}");
+                Console.WriteLine($"The way is: {countOfClimb * 3}");
+            }
+
+        }
+               
+    }
+}
